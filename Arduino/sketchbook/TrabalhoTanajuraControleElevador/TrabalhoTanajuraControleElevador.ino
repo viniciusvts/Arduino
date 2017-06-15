f20a0d212c1c3135e8560f72d4d1849a56f1fa8b
Vinicius de Santana <vinicius.vts@gmail.com>
2017-06-15 18:44:15 -0300

    Feito a contagem de andares

--- Arduino/sketchbook/TrabalhoTanajuraControleElevador/TrabalhoTanajuraControleElevador.ino
+++ Arduino/sketchbook/TrabalhoTanajuraControleElevador/TrabalhoTanajuraControleElevador.ino
@@ -8,10 +8,10 @@ const int pinBotao1 = 8;
 const int pinBotao2 = 9;
 const int pinSensorPresenca = 10;
 //controles
-int controlSensorPresenca = HIGH; //Ler Sensor de presença e lanca aqui
-int controlAndar = 0; //Controla o andar
+int controlSensorPresenca = 0; //controla o loop do sensor de presença
+int controlVerificaAndar = 0; //Controla o andar
 
-int controlTempoTroca = 60; //Lia pot e lancava aqui aqui, agora set um valor
+int controlTempoTroca = 200; //Lia pot e lancava aqui aqui, agora set um valor
 long unsigned int controlUltimoTempoLido = 0; //Ultimo tempo lido no millis
 
 int controlSentido = 1; //controla o sentido dos leds
@@ -20,10 +20,12 @@ int controlArrayLeds = 0; //controla a posição no array dos leds
 //quando 1 acende led, quando 0 apaga led
 int controlLoop = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
-  for(int x=0; x<4; x++){
+  for(int x=0; x<4; x++)
+  {
     pinMode(pinLED[x], OUTPUT);
   }
   pinMode(pinClock, OUTPUT);
@@ -32,48 +34,49 @@ void setup() {
   pinMode(pinSensorPresenca, INPUT);
 }
 
-void loop() {
+void loop()
+{
+  Serial.println(controlVerificaAndar);
   lerHardware();
-  Serial.println("leu hard");
 
-  if(isTime() == 1){
-    Serial.println("verificou tempo");
-    if(controlLoop == 1){
-      Serial.println("verificou loop");
+  if(isTime() == 1)
+  {
+    if(controlLoop == 1)
+    {
       ligaLed();
-    }else{
+    }else
+    {
       deslLed();
       controleLed();
     }
-    Serial.println("Ssiu loop");
   }
-  Serial.println("saiu tempo");
-  
+  verificarAndar();
   controleLoop();
-  Serial.println("controle loop");
 }
 
 //Para fazer a leitura do Hardware
-void lerHardware(){
+void lerHardware()
+{
 
   //Ler os botoes de fim da trilha e mudar o controlSentido
   //Alterar a condição para LOW se o botão for intermitente ligado
-  if ( digitalRead(pinBotao1) == HIGH ){
+  if ( digitalRead(pinBotao1) == HIGH )
+  {
     controlSentido = 1;
-  }else if( digitalRead(pinBotao2) == HIGH ){
+  }else if( digitalRead(pinBotao2) == HIGH )
+  {
     controlSentido = -1;
   }
-
-  //Ler sensor de presenca e altera andar
-  controlSensorPresenca = digitalRead(pinSensorPresenca);
 
   //Ler Potenciometro para a velocidade de execução da troca
   //controlTempoTroca = analogRead(pinPot);
 }
 
 //verifica millis
-int isTime(){
-  if( ( millis() - controlUltimoTempoLido ) > controlTempoTroca ){
+int isTime()
+{
+  if( ( millis() - controlUltimoTempoLido ) > controlTempoTroca )
+  {
     controlUltimoTempoLido = millis();
     return 1;
   }
@@ -81,37 +84,67 @@ int isTime(){
 }
 
 //me diz em que loop eu estou
-void controleLoop(){
-  if(controlLoop == 0){
+void controleLoop()
+{
+  if(controlLoop == 0)
+  {
     controlLoop = 1;
-  }else{
+  }else
+  {
     controlLoop = 0;
   }
 }
 
 //Liga o led
-void ligaLed(){
+void ligaLed()
+{
   digitalWrite(pinClock, HIGH);
   digitalWrite(pinLED[controlArrayLeds], HIGH);
 }
 //Desliga o led
-void deslLed(){
+void deslLed()
+{
   digitalWrite(pinClock, LOW);
   digitalWrite(pinLED[controlArrayLeds], LOW);
 }
 
 //Controla o sentido
-void controleLed(){
-  if(controlSentido == 1){
+void controleLed()
+{
+  if(controlSentido == 1)
+  {
     controlArrayLeds++;
-    if( controlArrayLeds > 3 ){
+    if( controlArrayLeds > 3 )
+    {
       controlArrayLeds = 0;
     }
-  }else if (controlSentido == -1){
+  }else if (controlSentido == -1)
+  {
     controlArrayLeds--;
-    if( controlArrayLeds < 0 ){
+    if( controlArrayLeds < 0 )
+    {
       controlArrayLeds = 3;
     }
   }
 }
 
+void verificarAndar()
+{
+  if( (digitalRead(pinSensorPresenca) == LOW ) && ( controlSensorPresenca == 0 ) )
+  {
+    controlSensorPresenca = 1; //controle para não contar mais de um andar por vez
+    
+    if(controlSentido == 1)
+    {
+      controlVerificaAndar++;
+    }else if (controlSentido == -1)
+    {
+      controlVerificaAndar--;
+    }
+  }
+
+  if( (digitalRead(pinSensorPresenca) == HIGH ) && ( controlSensorPresenca == 1 ) )
+  {
+    controlSensorPresenca = 0; //controle para não contar mais de um andar por vez
+  }
+}
