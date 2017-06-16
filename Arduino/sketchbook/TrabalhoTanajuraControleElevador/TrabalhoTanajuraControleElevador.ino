ece9237b5fdf3b3fd21622bc02cba9ee9fa2bc30
Vinicius de Santana <vinicius.vts@gmail.com>
2017-06-15 23:39:35 -0300

    Contagem e escolha de andares funcionando

--- Arduino/sketchbook/TrabalhoTanajuraControleElevador/TrabalhoTanajuraControleElevador.ino
+++ Arduino/sketchbook/TrabalhoTanajuraControleElevador/TrabalhoTanajuraControleElevador.ino
@@ -7,10 +7,29 @@ const int pinClock = 6;
 const int pinBotao1 = 8;
 const int pinBotao2 = 9;
 const int pinSensorPresenca = 10;
+
+//para os botões que pedem andares:
+const int andar1 = A0; //A0
+const int andar2 = A1; //A1
+const int andar3 = A2; //A2
+const int andar4 = A3; //A3
+const int andar5 = A4; //A4
+const int andar6 = A5; //A5
+
+const int VALORMEDIO = 1010;
+
+int andarvar1 = 0; //A0
+int andarvar2 = 0; //A1
+int andarvar3 = 0; //A2
+int andarvar4 = 0; //A3
+int andarvar5 = 0; //A4
+int andarvar6 = 0; //A5
+
 //controles
 int controlSensorPresenca = 0; //controla o loop do sensor de presença
 int controlVerificaAndar = 0; //Controla o andar
-
+int controlBotoesEscolhaAndar = 0; //controla o loop dos botoes de andar
+int controlAndarPedido = 0; //controla se estiver no andar pedido
 int controlTempoTroca = 200; //Lia pot e lancava aqui aqui, agora set um valor
 long unsigned int controlUltimoTempoLido = 0; //Ultimo tempo lido no millis
 
@@ -22,8 +41,10 @@ int controlLoop = 0;
 
 void setup()
 {
+  //Inicializo a porta serial
   Serial.begin(9600);
 
+  //inicializo os pinos
   for(int x=0; x<4; x++)
   {
     pinMode(pinLED[x], OUTPUT);
@@ -36,21 +57,29 @@ void setup()
 
 void loop()
 {
-  Serial.println(controlVerificaAndar);
+  Serial.println( controlVerificaAndar );
+  Serial.println( controlBotoesEscolhaAndar );
+  Serial.println( analogRead(andar1) );
+  Serial.println( analogRead(andar2) );
+  Serial.println( analogRead(andar3) );
   lerHardware();
 
-  if(isTime() == 1)
+  if( isTimeMotor() == 1 )
   {
-    if(controlLoop == 1)
+    if( controlLoop == 1 )
     {
       ligaLed();
-    }else
+    }else if(  controlLoop == 0 )
     {
       deslLed();
       controleLed();
+    }else
+    {
+      iteraAndarPedido();
     }
   }
-  verificarAndar();
+  contarAndar();
+  selecionaAndar();
   controleLoop();
 }
 
@@ -58,7 +87,7 @@ void loop()
 void lerHardware()
 {
 
-  //Ler os botoes de fim da trilha e mudar o controlSentido
+  //Ler os botoes de fim de trilha para mudar o controlSentido
   //Alterar a condição para LOW se o botão for intermitente ligado
   if ( digitalRead(pinBotao1) == HIGH )
   {
@@ -67,13 +96,21 @@ void lerHardware()
   {
     controlSentido = -1;
   }
-
+  //-------------------------------------------------------------------------------
   //Ler Potenciometro para a velocidade de execução da troca
   //controlTempoTroca = analogRead(pinPot);
+  //-------------------------------------------------------------------------------
+  //Ler se algum botao de andar esta pressionado
+  andarvar1 = analogRead( andar1 );
+  andarvar2 = analogRead( andar2 );
+  andarvar3 = analogRead( andar3 );
+  andarvar4 = analogRead( andar4 );
+  andarvar5 = analogRead( andar5 );
+  andarvar6 = analogRead( andar6 );
 }
 
 //verifica millis
-int isTime()
+int isTimeMotor()
 {
   if( ( millis() - controlUltimoTempoLido ) > controlTempoTroca )
   {
@@ -128,7 +165,8 @@ void controleLed()
   }
 }
 
-void verificarAndar()
+//verifica o andar e o atualiza na var controlSensorPresenca
+void contarAndar()
 {
   if( (digitalRead(pinSensorPresenca) == LOW ) && ( controlSensorPresenca == 0 ) )
   {
@@ -148,3 +186,51 @@ void verificarAndar()
     controlSensorPresenca = 0; //controle para não contar mais de um andar por vez
   }
 }
+
+//verifica se esta no andar que pediu
+int estaNoAndar()
+{
+  if(controlVerificaAndar == controlBotoesEscolhaAndar )
+  {
+    return 1;
+  }
+}
+
+void selecionaAndar()
+{
+  //->->prepare-se para meio mundo de if
+  if( andarvar1 > VALORMEDIO )
+  {
+    controlBotoesEscolhaAndar = 1;
+  }
+  if( andarvar2 > VALORMEDIO )
+  {
+    controlBotoesEscolhaAndar = 2;
+  }
+  if( andarvar3 > VALORMEDIO )
+  {
+    controlBotoesEscolhaAndar = 3;
+  }
+  if( andarvar4 >VALORMEDIO )
+  {
+    controlBotoesEscolhaAndar = 4;
+  }
+  if( andarvar5 > VALORMEDIO )
+  {
+    controlBotoesEscolhaAndar = 5;
+  }
+  if( andarvar6 > VALORMEDIO )
+  {
+    controlBotoesEscolhaAndar = 6;
+  }
+}
+
+void iteraAndarPedido()
+{
+  controlAndarPedido++;
+  if(controlAndarPedido > 8)
+  {
+    controlAndarPedido = 0;
+  }
+}
+
